fa61b5463135fbaaa155d08270a13c2bc6bec7f3
yaya1721 <Sonyahyun.hs@gmail.com>
2017-01-14 18:02:57 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ function addTask(){
   }
 }
 $("button#in").on("click", addTask);
+
+
+function isEnterPressed(event){
+  //alert(event.which);
+  if(event.which==13){
+   addTask();  
+  }
+}
+$("#ipt").on("keypress", isEnterPressed);
+
+
+
+
+
+    $("body").on("click","#button", function(){
+        if( $("#one-checkbox").prop("checked") ){ // 回傳布林值
+            alert( "你是"+$("#one-checkbox").val() );
+        } else {
+            alert( "你不是"+$("#one-checkbox").val() );
+        }
+    });
+
