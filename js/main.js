43737dc56f134a530489c5b2d3c59ada49494f13
yaya1721 <Sonyahyun.hs@gmail.com>
2017-01-14 16:52:09 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-<input type=”text” value=”Orange” />
+$(selector).val();
+function addTask(){
+  var value = $("#ipt").val();
+  if (value!==""){
+     $("#info").append(" <li><label><span class=\"content\">"
+                         +"Hello"+value+"</span></li>")
+  
+  
+    $("#ipt").val("");
+    updateNum();
+  }else{
+    alert("You must write something!");
+  }
+}
+$("#in").on("click", addTask);
