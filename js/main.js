40f760c1e300e75f514c032730119805f3a58219
yaya1721 <sonyahyun.hs@gmail.com>
2021-06-04 15:05:05 +0800

    hexo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-// install BootstrapVue
-Vue.use(BootstrapVue);
-// install BootstrapVue icon components plugin (optional)
-Vue.use(IconsPlugin);
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 function addTask(){
   var value = $("#ipt").val();
